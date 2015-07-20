bd501be4c02af988e8963b4305d9eefc7d392d48
Mathieu Harharidis <hhds.mathieu@gmail.com>
2015-07-20 21:33:49 +0200

    Ajout du projet C++

---
+++ OpenGLBattleArmy/include/configs.hpp
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_HPP_
+#define _CONFIG_HPP_
+
+//mieux d'utiliser un singleton
+
+class Configs{
+	public:
+		const static int WIDTH = 100;
+		const static int HEIGHT = 100;
+};
+#endif
